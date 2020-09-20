99d3040a0190309e2afa1ad8168263da61324bdd
Nick Guimarais <Nick.guimarais@gmail.com>
2020-09-19 18:31:43 -0700

    updated server.js

--- models/Exercise_model.js
+++ models/Exercise_model.js
@@ -4,15 +4,68 @@ const Schema = mongoose.Schema;
 
 const excerciseSchema = new Schema({
 
-    workoutType:  String,
-    workoutName: String,
-    workoutDistance: Number,
-    duration: Number,
-    weight: Number,
-    sets: Number,
-    reps: Number,
+    day: {
+        type: Date,
+        default: Date.now
+      },
+      exercises: [
+        {
+          type: {
+            type: String
+          },
+          name: {
+            type: String
+          },
+          duration: {
+            type: Number,
+            default: 0
+          },
+          weight: {
+            type: Number
+          },
+          reps: {
+            type: Number
+          }, 
+          sets: {
+            type: Number
+          },
+        }
+      ], 
+    //   totalDuration: Number
+
+    
+    // workoutType:  String,
+    // workoutName: String,
+    // workoutDistance: Number,
+    // duration: Number,
+    // weight: Number,
+    // sets: Number,
+    // reps: Number,
 
 });
+
+// excerciseSchema.methods.setTotalDuration = function() {
+  
+//     this.totalDuration = 0;
+//     console.log(this.exercises);
+//     this.exercises.forEach(exercise => {
+//       this.totalDuration += exercise.duration
+//     });
+//     return this.totalDuration;
+//   }
+
+
+
+
+excerciseSchema.virtual("totalDuration").get(function () {
+    // "reduce" array of exercises down to just the sum of their durations
+    return this.exercises.reduce((total, exercise) => {
+      return total + exercise.duration;
+    }, 0);
+  });
+
+
+
 
 const Exercise = mongoose.model("Exercise", excerciseSchema);
 
--- server.js
+++ server.js
@@ -2,12 +2,10 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-// const Excercise = require("./models/Excercise_model");
-
-
 const PORT = process.env.PORT || 3000;
 
 const db = require("./models");
+const { Exercise } = require("./models");
 
 const app = express();
 
@@ -18,20 +16,26 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
 
 
 require("./public/html-routes")(app);
 
 
 // create workouts
+
+
+app.post("/api/workouts", (req, res) => {
 db.Exercise.create({})
-    .then(dbexcercise => {
-        console.log(dbexcercise)
+    .then(data => {
+        res.json(data)
+        console.log(data)
     }) 
-    .catch(({ message }) => {
-        console.log(message);
-      });
+    .catch(err => {
+        console.log(err);
+      })
+    })
+
 
 //get last workout
 app.get("/api/workouts" , (req, res) => {
@@ -43,17 +47,44 @@ app.get("/api/workouts" , (req, res) => {
         res.json(error)
     })
 })
+
 //add/insert excercise need to modify later
-app.post("/api/workouts/:id", (req, res) => {
-    db.Exercise.create(body)
-    .then(({id}) => db.findOneAndUpdate({}))
-    .then(dbexcercise => {
-        res.json(dbexcercise)
-    })
+app.put("/api/workouts/:id", ({body, params}, res) => {
+    // console.log(req.body)
+//    Exercise.setTotalDuration();
+    db.Exercise.update(
+        { _id: params.id },
+        { $push: { exercises: body }}
+      )
+      .then(dbWorkout => {
+          res.json(dbWorkout);
+      })
     .catch(err => {
         res.json(err);
     })
 })
+
+
+// app.put("/api/workouts/:id", ({body, params}, res) => {
+//     const workout = new Workout(body)
+//     workout.setTotalDuration();
+//     Workout.update(
+//       { _id: params.id },
+//       { $push: { exercises: body }}
+//     )
+//     .then(dbWorkout => {
+//         res.json(dbWorkout);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+//   });
+
+
+// { _id: params.id },
+// { $push: { exercises: body }}
+// .then((_id) => db.findOneAndUpdate({_id: req.params._id}, { $push: { exercises: req.body } }, { new: true }))
+// .then(({ _id }) => db.User.findOneAndUpdate({}, { $push: { notes: _id } }, { new: true }))
 
 
 //get workouts in Range, need to modify later
